3715590336e9ccb55d8819cbc8a51f5ea118a8b7
mahmoud azeezeh <mahmmoadaziza@gmail.com>
2025-05-25 19:54:11 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Language Toggle
+  const languageToggle = document.querySelector(".language-toggle")
+  const htmlElement = document.documentElement
+  const currentLang = document.querySelector(".current-lang")
+
+  // Language translations
+ 
+ 
+  }
+
+
+
+ 
+
+   
+
+  // Theme Toggle
+ 
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+  
+
+   
+
+  
+
+
+ 
+})
